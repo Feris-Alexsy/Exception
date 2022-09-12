0b700d6833c6fddeba25b8232a53466ee62913ec
Feris-Alexsy <ahmadlamborghiniavetador>
2022-09-12 10:56:59 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileReader;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            //Mencoba membaca bekas latihan.txt
+            File file = new File("D://latihan.txt");
+            FileReader fr = new FileReader(file);
+            // Jika berhasil maka tampilkan pesan
+            System.out.println("Read file berhasil");
+        } catch (Exception e) {
+            //jika terjadi kesalahn maka tampilkan pesan
+            System.out.println(e.getMessage());
+        }
+    }
+}
